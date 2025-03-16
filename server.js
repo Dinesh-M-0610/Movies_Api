5cf7701d491956fbf0c274faf200f59f4f74af09
Dinesh-M-0610 <dineshmurugesan0610@gmail.com>
2025-03-16 18:48:47 +0530

    Connection with mongo db successfull

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose')
 dotenv.config({path: './config.env'})
 
 const app = require('./app.js');
@@ -6,8 +7,17 @@ const app = require('./app.js');
 // console.log(app.get('env'));
 console.log(process.env);
 
+console.log("MongoDB Connection String:", process.env.CONN_STR);
+
+mongoose.connect(process.env.CONN_STR,{
+    useNewUrlParser: true
+}).then((conn)=>{
+    console.log(conn);
+    console.log('DB Connection successfull')
+})
+
 //Create a server
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () =>{
     console.log("server has started...");
