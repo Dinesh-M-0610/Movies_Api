28a0aa20429b2542501a314f6e9bf877f6770c60
Dinesh-M-0610 <dineshmurugesan0610@gmail.com>
2025-03-17 16:30:03 +0530

    Modified the schema and added datas

--- Models/movieModel.js
+++ Models/movieModel.js
@@ -4,17 +4,52 @@ const movieSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, "Name is required field"],
-        unique: true
+        unique: true,
+        trim: true
     },
     description: String,
     duration: {
         type: Number,
-        required: [true, "Duration is required field"]
+        required: [true, "Duration is required field"],
+        trim: true
     },
     ratings: {
-        type: Number,
-        default: 5.0
+        type: Number
     },
+    totalRating: {
+        type: Number
+    },
+    releaseYear: {
+        type: Number,
+        required: [true, "Release year is required field"]
+    },
+    releaseDate: {
+        type: Date
+    },
+    createdAt: {
+        type: Date,
+        dafault: Date.now()
+    },
+    genres: {
+        type: [String],
+        required: [true, "Genres is required field"]
+    },
+    directors: {
+        type: [String],
+        required: [true, "directors is required field"]
+    },
+    coverImage: {
+        type: [String],
+        required: [true, "cover image is required field"]
+    },
+    actors: {
+        type: [String],
+        require: [true, 'actors is required field!']
+    },
+    price: {
+        type: Number,
+        require: [true, "price is required field"]
+    }
 })
 
 const Movie = mongoose.model('Movie', movieSchema);
